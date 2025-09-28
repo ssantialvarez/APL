da8bc7b0c6b5313c4da6a0d0596fc2aab5b37c6f
Santiago Alvarez <alvarezsanjavier@gmail.com>
2025-09-28 14:37:22 -0300

    ADD: distintas pruebas para inotify y git fetch

--- bash/ejercicio4/pruebaGitDiff.sh
+++ bash/ejercicio4/pruebaGitDiff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+DEBUG=false
 REPO="/home/sirius/Documents/virtualizacion/APL"
 CONFIG="./bash/ejercicio4/patrones.conf"
 LOG="./bash/ejercicio4/logs.log"
@@ -13,13 +13,14 @@ last_hash=$(git rev-parse "$BRANCH")
 while true; do
     git fetch origin "$BRANCH" >/dev/null 2>&1
     new_hash=$(git rev-parse "origin/$BRANCH")
-
+    $DEBUG && echo "LAST_HASH: $last_hash"
+    $DEBUG && echo "NEW HASH: $new_hash"
     if [ "$new_hash" != "$last_hash" ]; then
         echo "[$(date '+%F %T')] Detectados cambios en $BRANCH" >> "$LOG"
 
         # Archivos modificados entre commits
         changed_files=$(git diff --name-only "$last_hash" "$new_hash")
-        
+        $DEBUG && echo "CHANGED FILES: $changed_files"
         for file in $changed_files; do
             [ -f "$file" ] || continue
             while read pattern; do
