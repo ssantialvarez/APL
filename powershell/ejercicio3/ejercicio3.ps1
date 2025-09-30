c2470b21376cef07bcea674146b7cdd02ce20d93
Santiago Alvarez <alvarezsanjavier@gmail.com>
2025-09-30 14:14:35 -0300

    FIX: agregado Get-Help a ejercicio3 y verificacion de paremetros

--- powershell/ejercicio3/ejercicio3.ps1
+++ powershell/ejercicio3/ejercicio3.ps1
@@ -6,16 +6,53 @@
 # Script: ejercicio3.ps1
 # Uso: .\ejercicio3.ps1 usb,invalid
 
-if ($args.Count -eq 0) {
-    Write-Host "Uso: .\ejercicio3.ps1 palabra1 palabra2 ..."
-    exit 1
-}
+<#
+.SYNOPSIS
+Script para contar ocurrencias de palabras clave en archivos .log.
 
+.DESCRIPTION
+Este script busca archivos .log en el directorio actual y cuenta las ocurrencias de palabras clave especificadas como argumentos.
+
+.PARAMETER Directorio
+Especifica ruta del directorio con los archivos de logs a analizar
+.PARAMETER Palabras
+Lista de palabras clave a contabilizar, separadas por comas.
+
+.EXAMPLE
+\ejercicio3.ps1 usb,invalid
+Cuenta las ocurrencias de las palabras "usb" e "invalid" en los archivos .log del directorio actual.
+
+Ejemplo de archivo de entrada (system.log) 
+
+Aug 23 10:00:01 server.local kernel: [256.789] USB device plugged in. 
+Aug 23 10:00:05 server.local sshd[1234]: Invalid user from 192.168.1.1. 
+Aug 23 10:00:10 server.local sudo[5678]: Command not found. 
+Aug 23 10:00:15 server.local kernel: [258.123] USB device unplugged. 
+Aug 23 10:00:20 server.local sshd[1234]: Invalid user from 192.168.1.2. 
+ 
+Ejemplo de salida 
+USB: 2 
+Invalid: 2  
+ 
+.NOTES
+- El script requiere que existan archivos .log en el directorio actual.
+- Las palabras clave no distinguen entre mayúsculas y minúsculas.
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true, Position = 0, HelpMessage = 'Especifica ruta del directorio con los archivos de logs a analizar')]
+    [string]$Directorio,
+
+    [Parameter(Mandatory=$true, HelpMessage = 'Lista de palabras clave a contabilizar, separadas por comas.')]
+    [string[]]$Palabras
+)
 # Las palabras clave son los argumentos
-$keywordArray = $args
+$keywordArray = $Palabras
 
 # Buscar archivos .log en el directorio actual
-$logFiles = Get-ChildItem -Path . -Filter *.log
+$logFiles = Get-ChildItem -Path $Directorio -Filter *.log
 
 if ($logFiles.Count -eq 0) {
     Write-Host "No se encontraron archivos .log en el directorio."
